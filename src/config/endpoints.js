fd626aa1d67cce1fdbcad4992ba18cb628e7686a
Chad Ellison <chad.ellison0123@gmail.com>
2018-05-14 02:04:29 -0500

    Update api host

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,3 +1,3 @@
-export const API_HOST = window.location.hostname === 'localhost' ? 'http://localhost:3001' : 'https://tx-holdem-server.herokuapp.com'
+export const API_HOST = window.location.hostname === 'localhost' ? 'http://localhost:3001' : 'https://chess-engine-server.herokuapp.com'
 
-export const WEBSOCKET_HOST = window.location.hostname === 'localhost' ? 'ws://localhost:3001/cable' : 'wss://tx-holdem-server.herokuapp.com/cable'
+export const WEBSOCKET_HOST = window.location.hostname === 'localhost' ? 'ws://localhost:3001/cable' : 'wss://chess-engine-server.herokuapp.com/cable'
