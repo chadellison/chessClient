d6f3fe71134e92fbea13fa65d7b599d44264a864
Chad Ellison <ChadEllison@Erins-iMac.local>
2018-05-20 16:30:58 -0500

    remove commented out code.

--- src/sagas/apiHelper.js
+++ src/sagas/apiHelper.js
@@ -3,7 +3,7 @@ import {API_HOST} from "../config/endpoints.js"
 const HEADERS = {'Accept': 'application/json, text/plain, */*', 'Content-Type': 'application/json'}
 
 export const fetchGames = async () => {
-  const response = await fetch(`${API_HOST}/api/v1/games`, {
+  const response = await fetch(`${API_HOST}/`, {
     method: 'GET',
     headers: HEADERS
   })
