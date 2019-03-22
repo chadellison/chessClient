cb907dc5b6ecb21150f35d00c937312f4930b52e
Chad Ellison <chad.ellison0123@gmail.com>
2019-03-21 21:48:14 -0500

    add line chart graph

--- src/components/AnalyticsLineChart.js
+++ src/components/AnalyticsLineChart.js
@@ -11,14 +11,17 @@ class AnalyticsLineChart extends Component {
     return (
       <div hidden={!this.props.analytics.active} className="col-md-9 lineChart">
         <LineChart width={window.innerWidth * 0.7} height={250} data={this.props.lineChartData}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }} onMouseMove={(e)=> console.log(e.activeLabel, 'hi')}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="move" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="weight" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="setupWeight" stroke="#8884d8" />
+          <Line type="monotone" dataKey="materialWeight" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="attackWeight" stroke="#e07b33" />
+          <Line type="monotone" dataKey="threatWeight" stroke="#dd3918" />
+          <Line type="monotone" dataKey="totalWeight" stroke="#1749dd" />
         </LineChart>
       </div>
     )
