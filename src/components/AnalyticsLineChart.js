0b3f9ebcec1333e1cff8cb2a4e11b509ab4ea173
Chad Ellison <chad.ellison0123@gmail.com>
2019-04-17 16:09:26 -0500

    update line chart

--- src/components/AnalyticsLineChart.js
+++ src/components/AnalyticsLineChart.js
@@ -28,9 +28,7 @@ class AnalyticsLineChart extends Component {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="white" stroke="#8884d8" />
-          <Line type="monotone" dataKey="black" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="draw" stroke="#e07b33" />
+          <Line type="monotone" dataKey="prediction" stroke="#8884d8" />
         </LineChart>
       </div>
     )
